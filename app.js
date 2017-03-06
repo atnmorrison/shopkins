8171c6aa99e657acfdbaba85d408ea4977855a0a
Scott <scott.morrison@stantive.com>
2017-03-06 00:50:29 -0500

    updated backslashes

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ app.set('db', massiveInstance);
 //setup the templating engine
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
-app.set('views', __dirname+'\\views');
+app.set('views', __dirname+'/views');
 
-app.use(express.static(__dirname+'\\public'));
+app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
