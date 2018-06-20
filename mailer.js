de54cf1c0ae1b0e21198e4ef6d6b476898c51be7
Scott <scott.morrison@stantive.com>
2018-06-19 22:29:43 -0400

    added the mailer module

---
+++ mailer.js
@@ -0,0 +1,12 @@
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+export.sendWelcomeEmail = function(userdata) {
+	const msg = {
+		to: userdata.email,
+		from: 'scott@morrisonlive.ca',
+		subject: 'Welcome to Shopkins Tradding Post!',
+		text: 'Make sure to setup your collection and we\'ll notify you when we find a trade match with your doubles',
+		html: '<strong>Make sure to setup your collection and we\'ll notify you when we find a trade match with your doubles</strong>'
+	};
+	sgMail.send(msg);
+}
